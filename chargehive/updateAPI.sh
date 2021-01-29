079853eb261a225f7755cc3e09c21965f9b51d8a
Steve Barrow <steve@infurio.com>
2021-01-29 11:26:36 +0000

    fixed api update working from all working dirs

--- chargehive/updateAPI.sh
+++ chargehive/updateAPI.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")"
 echo "Downloading api.proto from chargehive:"
 curl -O https://api.chargehive.com/api.proto
-
-echo
 echo "Creating Go api from proto.api:"
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src \
   -I$GOPATH/src/github.com/chargehive/proto \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway \
-  --gogo_out=plugins=grpc:.\
+  --gogo_out=plugins=grpc:. \
   api.proto
-
 echo "Updated!"
